1d98665e0774211b4ed3574f1c6e087fc5aab000
Jon Franchi <jon.franchi@gmail.com>
2017-03-07 23:53:18 -0600

    Made it work even though I hate accessing an unknown key.

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,4 +1,4 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
+  // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
@@ -10,20 +10,31 @@ function deck_o_cards() {
   // Make 52 card objects and store them in the "cards" array
   // Hint: use 2 for loops
 
+  for(var i = 0; i < values.length; i++) {
+    for(var j = 0; j < suits.length; j++){
+      cards.push({[values[i]]: suits[j]});
+    }
+  }
+
+
 
   // 2. Shuffle the cards
   // Hint: shuffle function is already defined below
-
+  shuffledCards = shuffle (cards);
 
   // Pull the top card from the newly shuffledCards
+  var topCard = shuffledCards[0];
+  var topSuit =  Object.keys(topCard);
 
 
   // 3. Print the results:
   // "The deck has {n} cards"
   // "The top card is the {value} of {suit}"
+  console.log('The deck has ' + cards.length + ' cards');
+  console.log('The top card is the ' + topSuit + ' of ' + topCard[topSuit]);
+
 
 }
-
 
 
 // Fisher-Yates Shuffle
@@ -47,3 +58,5 @@ function shuffle(array) {
 
     return array;
 }
+
+deck_o_cards();
