62536bb1ead2a8433ad2469f7718066f7ead79eb
crake <MATTATAT@Matthews-MacBook-Pro-3.local>
2017-04-30 18:01:46 -0500

    Deck-o-Cards Homework

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,5 +1,3 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
@@ -7,43 +5,46 @@ function deck_o_cards() {
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+for (var i = 0; i < suits.length; i++) {
+  for (var j = 0; j < values.length; j++){
+    card = {
+      suit: suits[i],
+      value: values[j],
+    };
+    cards.push(card);
+  }
+}
 
+  shuffledCards = shuffle(cards);
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+  topCard = shuffledCards[0];
 
+  topSuit = topCard[Object.keys(topCard)[0]];
+  topValue = topCard[Object.keys(topCard)[1]];
 
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
+console.log("The deck has " + cards.length + " cards");
+console.log("The first card is the " + topValue + " of " + topSuit);
 
 }
 
+deck_o_cards();
 
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
 function shuffle(array) {
     var counter = array.length, temp, index;
 
-    // While there are elements in the array
     while (counter > 0) {
-        // Pick a random index
+
         index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
+
         counter--;
 
-        // And swap the last element with it
+
         temp = array[counter];
         array[counter] = array[index];
         array[index] = temp;
     }
 
     return array;
+
 }
